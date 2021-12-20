51fdf1464d99b333ce5ee5f92fceedf9da3099e4
Filip Cudny <filip96zx@gmail.com>
2021-12-20 19:14:52 +0100

    patch: added apiURL for heroku

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
-  apiURL: 'http://localhost:5000/api',
+  //apiURL: 'http://localhost:5000/api',
+  apiURL:'https://clinic-registration-app.herokuapp.com/api',
   //apiURL: 'http://vps-63e72677.vps.ovh.net:8080/api',
 };
 
