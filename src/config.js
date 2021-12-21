5a9e4f1ff3975a1356ed108e0f2ce343fe1d3b6c
Filip Cudny <filip96zx@gmail.com>
2021-12-21 10:24:59 +0100

    fix: changed apiURL

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 const config = {
   apiURL: () => {
     if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-      return 'https://localhost:5001/api';
+      return 'https://clinic-registration-app.herokuapp.com/api';
+      //return 'https://localhost:5001/api';
     } else {
       return 'https://clinic-registration-app.herokuapp.com/api';
     }
